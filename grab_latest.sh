7f14b4f1c44287608626e4bdeca8133428029f3f
Kyelbek <k13lbm@gmail.com>
2021-03-08 18:36:31 +0000

    Script: Force overwrite binaries

--- grab_latest.sh
+++ grab_latest.sh
@@ -7,6 +7,6 @@ WebView=$(echo "$FILELIST" | grep "WebView")
 mkdir -p temp
 rm ./temp/*
 wget $WebView -P ./temp
-mv ./temp/arm64_SystemWebView.apk ./prebuilt/arm64/webview.apk
-mv ./temp/arm_SystemWebView.apk ./prebuilt/arm/webview.apk
-mv ./temp/x86_SystemWebView.apk ./prebuilt/x86/webview.apk
+mv -f ./temp/arm64_SystemWebView.apk ./prebuilt/arm64/webview.apk
+mv -f ./temp/arm_SystemWebView.apk ./prebuilt/arm/webview.apk
+mv -f ./temp/x86_SystemWebView.apk ./prebuilt/x86/webview.apk
